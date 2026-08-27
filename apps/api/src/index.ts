2afda28569f6f256c0e791ac7393d2a0abd927dc
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-27 16:47:00 +0100

    first commit v3

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,30 +1,48 @@
 import { Hono } from "hono";
-import auth from "./routes/api/auth";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import musicRoute from "./routes/api/music_route";
-import developerRoute from "./routes/api/doc";
+// import auth from "./routes/api/auth";
+// import { cors } from "hono/cors";
+// import { logger } from "hono/logger";
+// import musicRoute from "./routes/api/music_route";
+// import developerRoute from "./routes/api/doc";
 const app = new Hono();
 
-const allowedOrigins = ["http://localhost:4000", "http://localhost:3000"];
-app.use(logger());
-app.use(
-  "*",
-  cors({
-    origin: (origin) => {
-      if (!origin) return origin;
-      if (allowedOrigins.includes(origin)) return origin;
-      return "";
-    },
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
+// const allowedOrigins = ["http://localhost:4000", "http://localhost:3000"];
+// app.use(logger());
+// app.use(
+//   "*",
+//   cors({
+//     origin: (origin) => {
+//       if (!origin) return origin;
+//       if (allowedOrigins.includes(origin)) return origin;
+//       return "";
+//     },
+//     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   }),
+// );
+// app.get("/health", (c) => {
+//   return c.text("Hello Hono!");
+// });
+// app.route("/auth", auth);
+// app.route("/music/v1", musicRoute);
+// app.route("/api/v1", developerRoute);
+
 app.get("/health", (c) => {
-  return c.text("Hello Hono!");
+  return c.json({
+    success: true,
+    message: "Hono API is alive",
+  });
 });
-app.route("/auth", auth);
-app.route("/music/v1", musicRoute);
-app.route("/api/v1", developerRoute);
+
+app.onError((err, c) => {
+  return c.json(
+    {
+      success: false,
+      message: err.message,
+      data: null,
+    },
+    400,
+  );
+});
 export default app;
