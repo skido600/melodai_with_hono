1b6728cacfde4a62fcd45ef056599b79d188dab0
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-28 05:39:01 +0100

    music route check mb

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,26 +1,28 @@
 import DashboardClientLayout from "@/components/DashboardClientLayout";
-// import { cookies } from "next/headers";
-// import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL!;
-console.log("text env", backendUrl);
+
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const cookieStore = await cookies();
+  const cookieStore = await cookies();
 
   const res = await fetch(`${backendUrl}/auth/me`, {
     method: "GET",
-    credentials: "include",
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
   });
 
   const data = await res.json();
 
   if (!res.ok || !data.success) {
-    console.log(data, "error from protected");
-    // redirect("/");
+    console.log("Protected route error:", data);
+    redirect("/");
   }
 
   return <DashboardClientLayout>{children}</DashboardClientLayout>;
