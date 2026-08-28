f9d4bb896689fd538283c6abffe2cbca88b7e24b
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-28 03:47:25 +0100

    music route check v044

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,32 +1,33 @@
 import { Hono } from "hono";
 import auth from "./routes/api/auth";
-// import { cors } from "hono/cors";
-// import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import musicRoute from "./routes/api/music_route";
-// import developerRoute from "./routes/api/doc";
+import developerRoute from "./routes/api/doc";
 const app = new Hono();
 
-// const allowedOrigins = ["http://localhost:4000", "http://localhost:3000"];
-// app.use(logger());
-// app.use(
-//   "*",
-//   cors({
-//     origin: (origin) => {
-//       if (!origin) return origin;
-//       if (allowedOrigins.includes(origin)) return origin;
-//       return "";
-//     },
-//     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   }),
-// );
-// app.get("/health", (c) => {
-//   return c.text("Hello Hono!");
-// });
+const allowedOrigins = [
+  "http://localhost:4000",
+  "https://melodai-cyan.vercel.app",
+];
+app.use(logger());
+app.use(
+  "*",
+  cors({
+    origin: (origin) => {
+      if (!origin) return origin;
+      if (allowedOrigins.includes(origin)) return origin;
+      return "";
+    },
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
 app.route("/auth", auth);
 app.route("/music/v1", musicRoute);
-// app.route("/api/v1", developerRoute);
+app.route("/api/v1", developerRoute);
 
 app.get("/health", (c) => {
   return c.json({
