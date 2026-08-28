d67636dad35f3affba96320cf5e330fe8e73ae7a
Chuks <63956312+skido600@users.noreply.github.com>
2026-08-28 05:39:42 +0100

    music route check mb

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import DashboardClientLayout from "@/components/DashboardClientLayout";
 import { cookies } from "next/headers";
-
+import { redirect } from "next/navigation";
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL!;
 
 export default async function DashboardLayout({
